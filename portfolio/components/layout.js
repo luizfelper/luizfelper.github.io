bb29330f4d9eb6955026cdcc844bed6e16e770e0
Felipe <luizfelper@gmail.com>
2021-10-27 15:36:48 -0300

    Configuração e estilização da pagina inicial

--- portfolio/components/layout.js
+++ portfolio/components/layout.js
@@ -1,8 +1,8 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
+import utilStyles from '../styles/utils.module.css';
 
 const name = 'Luiz Felipe';
 export const siteTitle = 'Next.js Blog'
