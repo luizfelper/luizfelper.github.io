a7a3b15e3fd93e8e796ff000f86174d63e485ca8
Felipe <luizfelper@gmail.com>
2021-10-27 14:53:00 -0300

    Estilização e criação dos components de layout

--- portfolio/pages/posts/first-post.js
+++ portfolio/pages/posts/first-post.js
@@ -1,15 +1,31 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import Image from 'next/image';
+import Layout from '../../components/layout'
 
 
 export default function FirstPost() {
     return (
         <>
-            <h1>First Post</h1>
-            <h2>
-                <Link href="/">
-                    <a>Back to Home</a>
-                </Link>
-            </h2>
+            <Layout>
+                <Head>
+                    <title>Blog</title>
+                </Head>
+                <h1>First Post</h1>
+                <h2>
+                    <Link href="/">
+                        <a>Back to Home</a>
+                    </Link>
+
+                    <Image
+                        src="/images/perfil.jpg"
+                        width={300}
+                        height={400}
+                        alt="Meu perfil"
+                    />
+
+                </h2>
+            </Layout>
         </>
     )
 }
